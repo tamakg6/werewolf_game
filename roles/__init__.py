44ab505fd4e3c637b65ecde58c1b0a3ffb0dd341
たま <tama.kg.6@gmail.com>
2026-01-28 01:36:42 +0900

    Create __init__.py

---
+++ roles/__init__.py
@@ -0,0 +1,7 @@
+from .villager import Villager
+from .werewolf import Werewolf
+from .seer import Seer
+from .guard import Guard
+from .medium import Medium
+from .madman import Madman
+from .fox import Fox
